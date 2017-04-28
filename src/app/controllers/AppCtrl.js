75bb386f538b9f1db22b410f9e0796122b967997
Guillaume MARDON <contact@gmardon.com>
2017-04-28 18:38:23 +0200

    close button

--- src/app/controllers/AppCtrl.js
+++ src/app/controllers/AppCtrl.js
@@ -1,6 +1,8 @@
+var remote = require("electron").remote;
+
 angular.controller('AppCtrl', function ($rootScope, BotManager, NetworkManager) { // ce controlleur n'est accessible que si tu es connecté (je bypass la connexion pour l'instant, mais dans ce contexte tu es secure)
         $rootScope.close = function () {
-            remote.getCurrentWindow().close();
+             remote.getCurrentWindow().close();
         };
 
         // en gros, le $scope c'est la variable qui sers pour le contexte du controller et de la page (la page html)/
